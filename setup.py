1dea4d7c0f1cb251c70da4ec1073667683335e05
-T.K.- <t_k_233@outlook.com>
2023-02-11 14:46:54 -0800

    FIX: fix project website

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="cc.xboxcontroller",
-    version="0.0.1",
+    version="0.0.2",
     author="Uncertainty.",
     author_email="t_k_233@outlook.email",
     description="Getting input from Microsoft XBox 360 controllers via the XInput library on Windows.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/uncertainty-cc/dotXboxController",
+    url="https://github.com/uncertainty-cc/XboxController-Python",
     project_urls={
         
     },
